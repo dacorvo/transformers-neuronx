21d84db80a33ce6f2966bb564c0a30704c0e738e
David Corvoysier <david@huggingface.co>
2025-01-28 15:08:38 +0000

    refactor: remove unused attributes

--- src/transformers_neuronx/llama/model.py
+++ src/transformers_neuronx/llama/model.py
@@ -30,8 +30,6 @@ class LlamaHloModel(NeuronHloDecoderModel):
     ):
         dtype = to_torch_dtype(neuron_config.amp)
         super().__init__(LlamaForCausalLM, config, dtype)
-        self.context_pre_hook = None
-        self.context_hook = None
         self.config = config
         self.neuron_config = neuron_config if neuron_config else NeuronConfig()
         hlo_builder = LlamaForSamplingNoEmbeddingHlo(
